79cb3027349f1611c044d11ac696d9ece91b8507
Kim-RedSentence <kim@redsentence.co.uk>
2024-10-09 17:05:32 +0100

    eop

---
+++ wp-content/themes/Northbanks/parts/orbital_slider.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="container__inner">
+        <div class="orbital orbital__outer">
+
+        </div>
+        <div class="orbital orbital__inner">
+            
+        </div>
+    </div>
+</div>
